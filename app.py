ee52214ee81b04122573e432c9b2dbf05bdd9124
lucaZH <lucarandria1@gmail.com>
2024-01-28 15:33:59 +0300

    add dera list hira

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from utils.pagination import FihiranapPagination
+import json
+from utils.Pagination import FihiranapPagination
+from utils.HiraBoky import HiraBoky
 
 url_dera = "https://katolika.org/fihirana/boky/show/4"
-Pagination = FihiranapPagination(url_dera)
+pagination = FihiranapPagination(url_dera)
+pages = pagination.generate_pagination_links()
+list_hira = []
 
-print(Pagination.generate_pagination_links())
+for page in pages:
+    katolika_scraper = HiraBoky(page, list_hira)
+    links_info_list = katolika_scraper.scrape_links_info()
+
+output = "./results/dera.json"
+with open(output, 'w', encoding='utf-8') as json_file:
+    json.dump(list_hira, json_file, ensure_ascii=False, indent=4)
