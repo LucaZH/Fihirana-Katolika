1798eccd0eb0560bf27a42484fdadbc339b0700b
lucaZH <lucarandria1@gmail.com>
2024-01-29 04:38:54 +0300

    Get list of hira

--- app.py
+++ app.py
@@ -2,15 +2,26 @@ import json
 from utils.Pagination import FihiranapPagination
 from utils.HiraBoky import HiraBoky
 
-url_dera = "https://katolika.org/fihirana/boky/show/4"
-pagination = FihiranapPagination(url_dera)
-pages = pagination.generate_pagination_links()
-list_hira = []
+with open('./utils/Fihirana.json', 'r', encoding='utf-8') as json_file:
+    json_data = json.load(json_file)
 
-for page in pages:
-    katolika_scraper = HiraBoky(page, list_hira)
-    links_info_list = katolika_scraper.scrape_links_info()
+for hira_info in json_data:
+    link = hira_info["link"]
+    title = hira_info["title"]
+    
+    pagination = FihiranapPagination(link)
+    pages = pagination.generate_pagination_links()
 
-output = "./results/dera.json"
-with open(output, 'w', encoding='utf-8') as json_file:
-    json.dump(list_hira, json_file, ensure_ascii=False, indent=4)
+    list_hira = []
+    for page in pages:
+        katolika_scraper = HiraBoky(page, list_hira)
+        links_info_list = katolika_scraper.scrape_links_info()
+
+    output_filename = f"./results/{title.lower().replace(' ', '_')}.json"
+
+    with open(output_filename, 'w', encoding='utf-8') as json_file:
+        json.dump(list_hira, json_file, ensure_ascii=False, indent=4)
+
+    print(f"Result for {title} has been written to {output_filename}")
+
+    list_hira.clear
