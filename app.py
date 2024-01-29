1a5b239173dfa15af088db44caef1602e966d225
lucaZH <lucarandria1@gmail.com>
2024-01-29 16:49:44 +0300

    Get reults

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-import json
+import json,os
+
+import jsonlines
 from utils.Pagination import FihiranapPagination
 from utils.HiraBoky import HiraBoky
+from utils.TononKira import TononKira
 
 def scrape_and_save_hira_list(json_path='./utils/Fihirana.json', output_dir='./results/list-hira'):
     with open(json_path, 'r', encoding='utf-8') as json_file:
@@ -26,3 +29,37 @@ def scrape_and_save_hira_list(json_path='./utils/Fihirana.json', output_dir='./r
         print(f"Result for {title} has been written to {output_filename}")
 
         list_hira.clear()
+
+def read_json_files(directory='./results/list-hira'):
+    json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
+
+    for json_file in json_files:
+        file_path = os.path.join(directory, json_file)
+        title_boky = os.path.splitext(file_path)[0].split('/')[-1]
+        with open(file_path, 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            for element in json_data:
+                write_hira_into_json('./results/hira',element["link"],title_boky)
+
+def write_hira_into_json(output_dir, url, fihirana_title):
+    output_filename = f"{output_dir}/{fihirana_title}"
+    hira = TononKira(url)
+    hira_content = hira.extract_hira_show_content()
+    entry = {
+        'title': hira_content[0],
+        'content': hira_content[1]
+    }
+
+    if os.path.exists(output_filename):
+        with open(output_filename, 'r', encoding='utf-8') as json_file:
+            existing_data = json.load(json_file)
+    else:
+        existing_data = []
+
+    existing_data.append(entry)
+
+    with open(output_filename, 'w', encoding='utf-8') as json_file:
+        json.dump(existing_data, json_file, ensure_ascii=False, indent=2)
+
+
+read_json_files()
