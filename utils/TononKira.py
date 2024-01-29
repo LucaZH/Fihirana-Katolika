4147e680aff32bf6109a1ea184211197005464e0
lucaZH <lucarandria1@gmail.com>
2024-01-29 16:49:30 +0300

    Modif structures

--- utils/TononKira.py
+++ utils/TononKira.py
@@ -4,24 +4,22 @@ from bs4 import BeautifulSoup
 class TononKira:
     def __init__(self, url):
         self.url = url
-        self.html_content = None
-        self.soup = None
 
     def fetch_html_content(self):
         response = requests.get(self.url)
-        self.html_content = response.content
+        return response.content
 
-    def parse_html_content(self):
-        if self.html_content:
-            self.soup = BeautifulSoup(self.html_content, 'html.parser')
+    def parse_html_content(self,html_content):
+        return BeautifulSoup(html_content, 'html.parser')
 
     def extract_hira_show_content(self):
-        if self.soup:
-            hira_show_div = self.soup.find('div', class_='hira_show')
-            if hira_show_div:
-                title = hira_show_div.find('h1').get_text(strip=True)
-                paragraphs = [p.get_text(strip=True) for p in hira_show_div.find_all('p')]
+        html_content = self.fetch_html_content()
+        soup = self.parse_html_content(html_content)
+        hira_show_div = soup.find('div', class_='hira_show')
+        if hira_show_div:
+            title = hira_show_div.find('h1').get_text(strip=True)
+            paragraphs = [p.get_text(strip=True) for p in hira_show_div.find_all('p')]
 
-                return title, paragraphs
+            return title, paragraphs
 
         return None
