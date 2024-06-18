6a8b9140fc05dd824d40a52b3b030fbe39b636f2
lucaZH <lucarandria1@gmail.com>
2024-06-18 15:21:58 +0300

    Refactor main.tsx to use AppRoutes instead of RouterProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import AppRoutes from "./AppRoutes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppRoutes />
   </React.StrictMode>
 );
