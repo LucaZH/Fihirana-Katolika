8ab270dd0acfcb642cbdd01a786865db197d9ea0
lucaZH <lucarandria1@gmail.com>
2024-06-18 15:39:09 +0300

    Remove unused Hira component from PHira page

--- frontend/src/page/PHira.tsx
+++ frontend/src/page/PHira.tsx
@@ -1,14 +1,9 @@
 import Fihirana from "../components/Fihirana";
-import Hira from "../components/Hira";
 
 function PHira() {
   return (
     <>
       <Fihirana fihirana="fihirana_dera" />
-      {/* <Hira
-        fihirana="fihirana_dera"
-        hira="Tsy ny (ahiahy) ahiahin'izao tontolo izao"
-      /> */}
     </>
   );
 }
