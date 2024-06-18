5e34bb43db7310dbae871db64bd69aeeeb4e2886
lucaZH <lucarandria1@gmail.com>
2024-06-18 15:21:31 +0300

    Refactor routing configuration

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,17 @@
+import PHira from "./page/PHira";
+import HiraWrapper from "./page/HiraWrapper";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PHira />} />
+        <Route path="/hira/:fihirana/:hira" element={<HiraWrapper />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
--- frontend/src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Hira from "./page/Hira";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Hira />,
-  },
-]);
