f99f0852f9e83f3f4501cf581c496677781bec50
lucaZH <lucarandria1@gmail.com>
2024-06-18 16:42:07 +0300

    Add tailwind

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,14 +1,15 @@
-import PHira from "./page/PHira";
 import HiraWrapper from "./page/HiraWrapper";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListFihirana from "./components/ListFihirana";
+import FihiranaWrapper from "./page/FihiranaWrapper";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PHira />} />
+        <Route path="/" element={<ListFihirana />} />
         <Route path="/:fihirana/:hira" element={<HiraWrapper />} />
+        <Route path="/:fihirana" element={<FihiranaWrapper />} />
       </Routes>
     </BrowserRouter>
   );
