2a12a872aa2def5671953aa923a3da6f8bf00335
lucaZH <lucarandria1@gmail.com>
2024-06-18 10:05:51 +0300

    Add Fihirana component to display a list of Fihirana

---
+++ frontend/src/components/Fihirana.tsx
@@ -0,0 +1,33 @@
+import { ReactNode, useEffect, useState } from "react";
+
+const API_URL = "http://localhost:5000/api/fihirana/";
+
+function Fihirana() {
+  const [fihirana, setFihirana] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setFihirana(data.data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Liste des Fihirana</h1>
+      <ul>
+        {fihirana.map((boky, index) => (
+          <li key={index}>{Object.values(boky)[0] as ReactNode}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Fihirana;
